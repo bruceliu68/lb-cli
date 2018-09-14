2ee9c430c4e63ecc66c235eceea4692d6187cf20
wyr <wyr@wyrdeMacBook-Pro.local>
2018-09-14 16:08:05 +0800

    feat:新增条件

--- index.js
+++ index.js
@@ -12,39 +12,48 @@ program.version('1.0.0', '-v, --version')
     .command('init <name>')
     .action((name) => {
         if(!fs.existsSync(name)){
-            inquirer.prompt([
-                {
-                    name: 'description',
-                    message: '请输入项目描述'
-                },
-                {
-                    name: 'author',
-                    message: '请输入作者名称'
-                }
-            ]).then((answers) => {
-                const spinner = ora('正在下载模板...');
-                spinner.start();
-                download('https://github.com:bruceliu68/vuedemo#master', name, {clone: true}, (err) => {
-                    if(err){
-                        spinner.fail();
-                        console.log(symbols.error, chalk.red(err));
-                    }else{
-                        spinner.succeed();
-                        const fileName = `${name}/package.json`;
-                        const meta = {
-                            name,
-                            description: answers.description,
-                            author: answers.author
-                        }
-                        if(fs.existsSync(fileName)){
-                            const content = fs.readFileSync(fileName).toString();
-                            const result = handlebars.compile(content)(meta);
-                            fs.writeFileSync(fileName, result);
-                        }
-                        console.log(symbols.success, chalk.green('项目初始化完成'));
-                    }
-                })
-            })
+            inquirer.prompt([
+                {
+                    name: 'comfirm',
+                    message: '下载宇宙最强架构（yes/no）'
+                }
+            ]).then((answers) => {
+                const comfirm = answers.comfirm.toLocaleLowerCase();
+                if (comfirm !== 'yes' && comfirm !== 'y') return;
+                inquirer.prompt([
+                   {
+                       name: 'description',
+                       message: '请输入项目描述'
+                   },
+                   {
+                       name: 'author',
+                       message: '请输入作者名称'
+                   }
+               ]).then((answers) => {
+                   const spinner = ora('正在下载模板...');
+                   spinner.start();
+                   download('https://github.com:bruceliu68/vuedemo#master', name, {clone: true}, (err) => {
+                       if(err){
+                           spinner.fail();
+                           console.log(symbols.error, chalk.red(err));
+                       }else{
+                           spinner.succeed();
+                           const fileName = `${name}/package.json`;
+                           const meta = {
+                               name,
+                               description: answers.description,
+                               author: answers.author
+                           }
+                           if(fs.existsSync(fileName)){
+                               const content = fs.readFileSync(fileName).toString();
+                               const result = handlebars.compile(content)(meta);
+                               fs.writeFileSync(fileName, result);
+                           }
+                           console.log(symbols.success, chalk.green('项目初始化完成'));
+                       }
+                   })
+               })
+            })
         }else{
             // 错误提示项目已存在，避免覆盖原有项目
             console.log(symbols.error, chalk.red('项目已存在'));
